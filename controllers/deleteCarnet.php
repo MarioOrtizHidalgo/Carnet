f7b6679ed540f7a32ef71307ce558ac4ef2769d4
Mario Ortiz <ortizhidalgomario@gmail.com>
2024-04-11 12:35:23 +0200

    Eliminar Datos Solucionado

--- controllers/deleteCarnet.php
+++ controllers/deleteCarnet.php
@@ -2,23 +2,15 @@
 
 require_once('../models/conectDatebase.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET['id']) && !empty($_GET['id'])) {
 
         $id = $_GET['id'];
         $borrar = $conexion->query("DELETE FROM Carnet WHERE id=$id");
 
-        var_dump( $borrar );
-
-        if ($borrar == 1) {
-            echo "<div class='alert alert-success'>Se ha borrado correctamente el conductor</div>";
-        } else {
-            echo "<div class='alert alert-danger'>No se ha podido borrar el conductor</div>";
-        }
+        header("Location: ../index.php");
 
     } else {
         echo "Ha habido un error";
     }
-}
 
 ?>
